4e6f719601ea4a5b3f386585d6c5d59b9ed01949
matiushariman <matius.hariman@gmail.com>
2022-05-07 17:54:05 +0800

    :sparkles: add Spotify login URL

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,30 +3,54 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-
 import { Helmet } from 'components/Helmet';
 
 import { Header } from './Header';
 
-const HomePage = () => (
-  <>
-    <Helmet title="Home" />
-    <Header />
-    <Container
-      sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-around', flexGrow: 1 }}
-    >
-      <Box>
-        <Stack direction="column" spacing={4}>
-          <Typography variant="h1" align="center">
-            React Spotify Clone Webapp
-          </Typography>
-          <Box display="inline-flex" justifyContent="space-around">
-            <Button size="large">Log in to Spotify</Button>
-          </Box>
-        </Stack>
-      </Box>
-    </Container>
-  </>
-);
+const SCOPE = [
+  'user-read-private',
+  'user-read-email',
+  'user-read-recently-played',
+  'user-top-read',
+  'user-follow-read',
+  'user-follow-modify',
+  'playlist-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+];
+
+const LOGIN_URL = `https://accounts.spotify.com/authorize?client_id=${
+  process.env.REACT_APP_CLIENT_ID
+}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=${SCOPE.join(' ')}&response_type=code`;
+
+const HomePage = () => {
+  // function to open Spotify Auth page
+  const handleRedirectToSpotify: () => void = () => {
+    window.open(LOGIN_URL, '_self');
+  };
+
+  return (
+    <>
+      <Helmet title="Home" />
+      <Header />
+      <Container
+        sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-around', flexGrow: 1 }}
+      >
+        <Box>
+          <Stack direction="column" spacing={4}>
+            <Typography variant="h1" align="center">
+              React Spotify Clone Webapp
+            </Typography>
+            <Box display="inline-flex" justifyContent="space-around">
+              <Button onClick={handleRedirectToSpotify} size="large">
+                Log in to Spotify
+              </Button>
+            </Box>
+          </Stack>
+        </Box>
+      </Container>
+    </>
+  );
+};
 
 export default HomePage;
