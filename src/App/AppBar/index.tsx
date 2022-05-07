bae4d8b45c9c273e4f675ff21e553e9b7de62b16
matiushariman <matius.hariman@gmail.com>
2022-05-07 17:17:12 +0800

    :fire: remove AppBar component

--- src/App/AppBar/index.tsx
+++
@@ -1,8 +0,0 @@
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-
-export const AppBar = (): JSX.Element => (
-  <MuiAppBar>
-    <Toolbar />
-  </MuiAppBar>
-);
